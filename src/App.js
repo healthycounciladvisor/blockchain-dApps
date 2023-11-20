936438a75c7cb79df3fe60d641b725a9c1b3e486
healthycounciladvisor <jimm.dev2323@gmail.com>
2023-11-20 16:30:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className='App-intro'>
+          Sven's <code>Todo-List</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          By React
+        </a>
+      </header>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
