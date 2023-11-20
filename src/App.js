baed6f0075c0d2ea67b50ed61d88e36a516c5fe3
healthycounciladvisor <jimm.dev2323@gmail.com>
2023-11-20 16:38:54 +0100

    update commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className='App-intro'>
-          Sven's <code>Todo-List</code>
+          A<code>Todo-List</code>
         </p>
         <a
           className="App-link"
